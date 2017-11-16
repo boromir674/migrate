833e731d51d421beb28b6467a673ac3122e4e3df
Konstantinos <k.lampridis@hotmail.com>
2017-11-16 22:06:31 +0100

    finish wego installer

--- install-wego.sh
+++ install-wego.sh
@@ -3,7 +3,8 @@
 binary=$1
 git_url=$2
 go_root_dir=$3
-the_alias=$4
+aliases_file=$4
+the_alias=$5
 
 echo $git_url
 
@@ -20,21 +21,24 @@ else
     apt-get install golang git > /dev/null && echo "Installed 'go' program"
 fi
 
-# echo $git_url
-# repo=${git_url:8:-4}
-# echo $repo
-
 regex="https?://(.*).git"
-echo $regex
+
 
 if [[ $git_url =~ $regex ]]; then
     repo="${BASH_REMATCH[1]}";
-    echo $repo
-    # repo=${repo/.git/}
-    # echo $repo
+    go get $repo >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "Go got repo '$repo' in GOPATH";
+        echo "alias $the_alias='$go_root_dir/bin/$binary'" >> $aliases_file && echo "Wrote alias in $aliases_file"
+        echo "Please source yourself and use as '$the_alias'"
+    else
+        echo "Go failed to get '$repo'"
+    fi
 else
-    echo "$git_url doesn't match" >&2
+    echo "'$git_url' doesn't match" >&2
 fi
 
-go get $repo && echo  "got repo '$repo' in GOPATH\n";
+
+
+
 
