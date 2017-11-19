c39352d5ba7e927bcd390ba7a8ab958927ca2cfe
Konstantinos <k.lampridis@hotmail.com>
2017-11-19 13:37:23 +0100

    fix alias writer

--- write-alias.sh
+++ write-alias.sh
@@ -4,7 +4,7 @@ the_alias=$1
 path=$2
 aliases_file=$3
 
-echo "alias $the_alias='$go_root_dir/bin/$binary'" >> $aliases_file
+echo "alias $the_alias='$path'" >> $aliases_file
 if [ $? -eq 0 ]; then
     echo "Wrote alias in $aliases_file. Please source yourself and use as '$the_alias'"
 else
