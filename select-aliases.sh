80fd4b3f5e09fb0704907a7ea15c2751643ee741
Konstantinos <k.lampridis@hotmail.com>
2017-11-21 01:38:48 +0100

    modify select-aliases to 'save' aliases by default in resources/selected_aliases

--- select-aliases.sh
+++ select-aliases.sh
@@ -3,15 +3,17 @@
 ### BUG: renders/inserts '\n' in places of '\' found (i.e. alias ltr=\'\ls -tr')
 
 me=`basename "$0"`
+dir_here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+target_file_name=selected_aliases
 
-if [ "$#" -ne 3 ]; then
-    echo "Example usage: $me ~/.bash_aliases /media$USER/a-disk/-a-path/ \"usefull optional must\""
+if [ "$#" -ne 2 ]; then
+    echo "Example usage: $me ~/.bash_aliases /media/$USER/a-disk/-a-path/ \"usefull optional must\""
     exit 1
 fi
 
 aliases_source=$1 # the file to search for defined aliases
-aliases_target=$2 # the file to write the extracted aliases to
-selected_aliases=$3 # comma separated flags indicating which aliases groups to extract
+selected_aliases=$2 # comma separated flags indicating which aliases groups to extract
+aliases_target=$dir_here/resources/$target_file_name # the file to write the extracted aliases to
 
 if [ -f $aliases_target ]; then rm $aliases_target; fi
 touch $aliases_target
@@ -26,9 +28,9 @@ for alias_type in "${alias_types[@]}"; do
         aliases_block="\n##### $alias_type #####${BASH_REMATCH[1]}# $alias_type #####\n"
         printf "$aliases_block" >> $aliases_target
         lines=`grep -E "^alias " $aliases_target | wc -l`
-        echo "Loaded $((lines-prev_lines)) '$alias_type' alias entries in '$aliases_target' from '$aliases_source'"
+        echo "Loaded $((lines-prev_lines)) '$alias_type' alias entries in 'resources/$target_file_name' from '$aliases_source'"
         prev_lines=$lines
     else
-        echo "Did not find '$alias_types' alias entries in '$aliases_source'"
+        echo "Did not find '$alias_type' alias header entries in '$aliases_source'"
     fi
 done
